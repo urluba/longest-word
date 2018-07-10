80653fb3e351e422847d9f76d4ae5b29e140c12a
urluba <lucas.barriere@gmail.com>
2018-07-10 16:06:12 +0200

    Added support for dictionnary

--- game.py
+++ game.py
@@ -3,6 +3,7 @@
 
 import logging
 import string
+import requests
 from copy import copy
 from random import choice
 
@@ -28,7 +29,27 @@ class Game:
 
         self.grid = Game._generate_grid(Game.grid_size)
 
+    @staticmethod
+    def is_in_dictionnary(word: list) -> bool:
+        try:
+            response = requests.get(f'https://wagon-dictionary.herokuapp.com/{word}').json()
+            was_found = response.get('found', False)
+        except Exception as exc:
+            logging.exception(exc)
+            return False
+
+        logging.debug(
+            '%s was %s found in dictionnary',
+            word,
+            ' ' if was_found else 'not '
+        )
+
+        return was_found
+
     def is_valid(self, word: list) -> bool:
+        if not Game.is_in_dictionnary(word):
+            return False
+
         grid = copy(self.grid)
 
         for letter in word:
